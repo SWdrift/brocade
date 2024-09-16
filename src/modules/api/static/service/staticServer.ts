0827a4fff10224a09ddb82e6d103d45b5f64b17a
CZXyee <CZXyee@163.com>
2024-09-17 02:18:46 +0800

    feat: 声明文件 utf8

--- src/modules/api/static/service/staticServer.ts
+++ src/modules/api/static/service/staticServer.ts
@@ -8,8 +8,9 @@ export function getStaticServer() {
         defer: false, // 找到文件后不执行之后的中间件
         setHeaders: (res, path, _stats) => {
             const mimeType = getMimeType(path);
+            const utf8MimeType = addUtf8Charset(mimeType);
             if (mimeType) {
-                res.setHeader("Content-Type", mimeType);
+                res.setHeader("Content-Type", utf8MimeType);
             }
         }
     });
@@ -17,5 +18,12 @@ export function getStaticServer() {
 
 function getMimeType(path: string) {
     const ext = `.${path.split(".").pop()}`;
-    return CONFIG.CUSTOM_MIME_TYPES[ext] || null;
+    return CONFIG.CUSTOM_MIME_TYPES[ext];
+}
+
+function addUtf8Charset(mimeType?: string) {
+    if (!mimeType) {
+        return "charset=UTF-8";
+    }
+    return `${mimeType}; charset=UTF-8`;
 }
